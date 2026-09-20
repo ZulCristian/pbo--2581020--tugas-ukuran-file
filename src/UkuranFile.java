ab3e3cc44d7e6896ae72221cf72f486fd431630f
Zul Cristian Purba <2581020@unai.edu>
2026-09-20 17:35:16 +0700

    menambahkan scanner

--- src/UkuranFile.java
+++ src/UkuranFile.java
@@ -4,12 +4,7 @@ public class UkuranFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Nama file      : ");
-        String namaFile = scanner.nextLine();
 
-        System.out.print("Ukuran (byte)  : ");
-        // kalau pakai int, jadinya overflow karena sudah melebihi batas int (2.147.483.647 byte / ~2 GB)
-        long ukuranByte = scanner.nextLong();
 
 
 
