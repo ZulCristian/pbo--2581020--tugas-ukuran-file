3e83881d75bf9bf65caf69ca830a6ea3ba83701f
Zul Cristian Purba <2581020@unai.edu>
2026-09-20 17:43:27 +0700

    menambahkan komentar

--- src/UkuranFile.java
+++ src/UkuranFile.java
@@ -4,6 +4,20 @@ public class UkuranFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Nama file      : ");
+        String namaFile = scanner.nextLine();
+
+        System.out.print("Ukuran (byte)  : ");
+        long ukuranByte = scanner.nextLong();
+        // kalau pakai int, jadinya overflow karena sudah melebihi batas int (2.147.483.647 byte)
+
+        /*
+         * Kenapa menggunakan 1024.0, bukan 1024?
+         * Jika menggunakan 1024, pembagian dilakukan sebagai bilangan bulat,
+         * sehingga angka desimal akan terpotong.
+         * Dengan 1024.0, Java menggunakan pembagian desimal,
+         * sehingga hasilnya tetap lebih akurat.
+         */
 
 
 
